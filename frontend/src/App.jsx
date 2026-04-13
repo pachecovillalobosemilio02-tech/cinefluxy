f1a6ee28cc9aaddcad52f1a9c9c224a7ca0c8fba
Steven Hernandez <yovngsteven22@gmail.com>
2026-04-12 22:57:59 -0500

    fix react imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './pages/Home'
